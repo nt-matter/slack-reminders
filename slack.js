a5f5200aa86d6322dc4fef794df554ee396a00ce
Ilán Vivanco <ilanvivanco@gmail.com>
2023-01-26 16:02:34 +0100

    Add Slack API connection

---
+++ slack.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const { WebClient, LogLevel } = require("@slack/web-api");
+const client = new WebClient(process.env.SLACK_TOKEN);
+
+// Send meesages to a channel on slack
+exports.sendMessage = async ({ text, channel }) => {
+	try {
+		const result = await client.chat.postMessage({
+			channel,
+			text
+		});
+	}
+	catch (error) {
+		console.error(error);
+	}
+}
