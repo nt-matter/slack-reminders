c2b45b1365444e53651cbc174e4a4d1965e966b2
Ilán Vivanco <ilanvivanco@gmail.com>
2023-01-26 16:02:45 +0100

    Add date utils

---
+++ dates.js
@@ -0,0 +1,21 @@
+const today = new Date();
+
+function dateFromString(dateString) {
+	const [d, m, y] = dateString.split(/[/-]/);
+
+	return new Date(y, m - 1, d);
+}
+
+exports.isTheDateToday = (dateString) => {
+	const date = dateFromString(dateString);
+
+	if (!isNaN(date.getTime()))
+		return (date.getMonth() == today.getMonth() && date.getDate() == today.getDate())
+}
+
+exports.getAge = (dateString) => {
+	const date = dateFromString(dateString);
+
+	if (!isNaN(date.getTime()))
+		return today.getFullYear() - date.getFullYear()
+}
