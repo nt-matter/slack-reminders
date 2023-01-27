2f6678461c696bc7fa6bf2f369781f710d5d913f
Ilán Vivanco <ilanvivanco@gmail.com>
2023-01-27 02:16:20 +0100

    Try with slack notify

--- slack.js
+++ slack.js
@@ -1,20 +1,19 @@
 require('dotenv').config();
-
-const { WebClient, LogLevel } = require("@slack/web-api");
-const client = new WebClient(process.env.SLACK_TOKEN, {
-	logLevel: LogLevel.DEBUG,
-	retryConfig: { retries: 3 }
-});
+const SlackNotify = require('slack-notify');
+const slack = SlackNotify(process.env.SLACK_WEBHOOK_URL);
 
 // Send meesages to a channel on slack
 exports.sendMessage = async ({ text, channel }) => {
-	try {
-		const result = await client.chat.postMessage({
-			channel,
-			text
-		});
-	}
-	catch (error) {
-		console.error(error);
-	}
+	console.log('--- Sending message ---')
+	console.log({ text, channel })
+
+	slack.send({
+		channel,
+		text,
+		icon_url: 'https://ntmatter.com/wp-content/uploads/2023/01/ntm-logo.jpg',
+		username: 'NTM Bot'
+	}).catch((err) => {
+		console.log('--- API error ---')
+		console.error(err);
+	});
 }
