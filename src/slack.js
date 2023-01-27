0e1186fdeb4396eb781444a2a17239d05a10a2c8
Ilán Vivanco <ilanvivanco@gmail.com>
2023-01-27 19:36:54 +0100

    Add new webhook to general channel

--- src/slack.js
+++ src/slack.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const axios = require('axios')
 
+// Generate Slack webhook from https://ntmatter.slack.com/apps/A0F7XDUAZ-incoming-webhooks?tab=settings
+const generalChannel = process.env.SLACK_GENERAL_WEBHOOK_URL
+const testChannel = process.env.SLACK_TEST_WEBHOOK_URL
+
 // Send meesages to a channel on slack
 exports.sendMessage = async (text) => {
 	console.log('--- Sending message ---')
@@ -8,7 +12,7 @@ exports.sendMessage = async (text) => {
 
 	const config = {
 		method: 'post',
-		url: process.env.SLACK_WEBHOOK_URL,
+		url: process.env.CONTEXT === 'dev' ? testChannel : generalChannel,
 		headers: { 'Content-Type': 'application/json' },
 		data: JSON.stringify({ text }),
 	}
