ea6164d55c3a26beb8bdc6097cf226b9f1afa93b
Ilán Vivanco <ilanvivanco@gmail.com>
2023-01-27 19:37:47 +0100

    Add real reminders

--- data/remindersList.js
+++ data/remindersList.js
@@ -2,7 +2,35 @@ exports.remindersList = [
 	// Extras
 	{ type: 'bot', date: '27/01/2023' },
 
-	// Anniversaries
-	{ type: 'birthday', name: 'Ilán', date: '17/06/1988', handler: '@ilan.vivanco' },
-	{ type: 'ntm', name: 'Ilán', date: '01/06/2015', handler: '@ilan.vivanco' },
+	// Birthdays
+	{ type: 'birthday', name: 'abril', date: '26/04/1993' },
+	{ type: 'birthday', name: 'federico', date: '31/03/1995' },
+	{ type: 'birthday', name: 'gustavo', date: '5/05/1987' },
+	{ type: 'birthday', name: 'ilan', date: '17/06/1988' },
+	{ type: 'birthday', name: 'javier', date: '3/12/1984' },
+	{ type: 'birthday', name: 'javierv', date: '26/04/2002' },
+	{ type: 'birthday', name: 'jeronimo', date: '6/12/1981' },
+	{ type: 'birthday', name: 'juanpablo', date: '13/06/1997' },
+	{ type: 'birthday', name: 'julian', date: '01/01/1970' },
+	{ type: 'birthday', name: 'leandro', date: '22/01/1998' },
+	{ type: 'birthday', name: 'luciano', date: '3/03/1987' },
+	{ type: 'birthday', name: 'mariela', date: '23/06/1990' },
+	{ type: 'birthday', name: 'roni', date: '27/03/1980' },
+	{ type: 'birthday', name: 'tatiana', date: '29/01/1990' },
+
+	// NTM Anniversaries
+	{ type: 'ntm', name: 'abril', date: '26/11/2019' },
+	{ type: 'ntm', name: 'federico', date: '04/03/2020' },
+	{ type: 'ntm', name: 'gustavo', date: '01/01/2023' },
+	{ type: 'ntm', name: 'ilan', date: '01/06/2015' },
+	{ type: 'ntm', name: 'javier', date: '01/02/2012' },
+	{ type: 'ntm', name: 'javierv', date: '01/01/2023' },
+	{ type: 'ntm', name: 'jeronimo', date: '10/04/2019' },
+	{ type: 'ntm', name: 'juanpablo', date: '12/07/2021' },
+	{ type: 'ntm', name: 'julian', date: '18/07/2022' },
+	{ type: 'ntm', name: 'leandro', date: '02/08/2022' },
+	{ type: 'ntm', name: 'luciano', date: '01/10/2020' },
+	{ type: 'ntm', name: 'mariela', date: '01/01/2018' },
+	{ type: 'ntm', name: 'roni', date: '01/01/1900' },
+	{ type: 'ntm', name: 'tatiana', date: '03/10/2022' },
 ]
