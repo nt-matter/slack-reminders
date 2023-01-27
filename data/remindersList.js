16862ebe8c417b8d52c8d39a245a4e0c6c1bda27
Ilán Vivanco <ilanvivanco@gmail.com>
2023-01-27 17:17:29 +0100

    Add NTM bot reminders

--- data/remindersList.js
+++ data/remindersList.js
@@ -1,6 +1,8 @@
 exports.remindersList = [
+	{ type: 'bot', name: 'NTN Bot', date: '27/01/2023' },
+	{ type: 'botday', name: 'NTN Bot', date: '27/01/2023' },
+
 	// Anniversaries
-	{ type: 'ntm', name: 'Test', date: '27/01/2022', handler: '@ilan.vivanco' },
 	{ type: 'birthday', name: 'Ilán', date: '17/06/1988', handler: '@ilan.vivanco' },
 	{ type: 'ntm', name: 'Ilán', date: '01/06/2015', handler: '@ilan.vivanco' },
 ]
--- src/reminders.js
+++ src/reminders.js
@@ -7,15 +7,20 @@ exports.checkForReminders = async () => {
 	const today = new Date()
 	const todaysReminders = remindersList.filter((reminder) => isToday(reminder.date))
 
+	console.log(todaysReminders)
+
 	for (const reminder of todaysReminders) {
 		const age = getAge(reminder.date)
-		const yearText = age > 1 ? 'años' : 'año'
+		const yearText = age == 1 ? 'año' : 'años'
 		const todayFormatted = today.toLocaleDateString('ES', {
 			month: 'long',
 			day: 'numeric',
 		})
 
 		const messages = {
+			bot: `📆 <!everyone>: Siendo *${todayFormatted}* queda oficialmente inaugurado el *NTM Bot* 🤖!! 🔥🎇`,
+			botday: `📆 <!everyone>: Un día como hoy hace ${age} ${yearText} cobraba vida el *NTM Bot* 🤖!! 🍰⌛`,
+
 			birthday: `📆 <!everyone>: Hoy *${todayFormatted}* <${reminder.handler}> cumple ${age} ${yearText}!! 🥳🎉🎂`,
 			ntm: `📆 <!everyone>: Hoy *${todayFormatted}* <${reminder.handler}> cumple ${age} ${yearText} trabajando en *NTM*!! 🥳🎉`,
 		}
